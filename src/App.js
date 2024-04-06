9b3a27ca8164c8b3acd5f87c55145d28b5376fee
ISAWASHUN <syun20000616@icloud.com>
2024-04-06 14:11:15 +0900

    ページにキャラクターを表示

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+
+
   useEffect(() => {
     fetchCharacters();
   }, [])
@@ -10,12 +14,31 @@ function App() {
   const fetchCharacters = async () => {
     const apiUrl = 'https://narutodb.xyz/api/character';
     const result = await axios.get(apiUrl);
-    console.log(result);
+    setCharacters(result.data.characters);
   }
 
   return (
-    <div className="App">
-      
+    <div className="container">
+      <main>
+        <div className='cards-container'>
+          {characters.map((character) => (
+            <div key={character.id} className='card'>
+              <img src={character.images[0] != null ? character.images[0] : 'dummy.png'} alt={character.name} />
+              <div className='card-content'>
+                <h3 className='card-title'>{character.name}</h3>
+                <p className='card-description'>
+                  {character.debut != null ? character.debut.appearsIn : 'No debut'}
+                </p>
+                <div className='card-footer'>
+                  <span className=''>
+                    {character.personal?.affiliation ?? 'No affiliation'}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
