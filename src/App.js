c47a664e77c84b064f2b95b192f7c948a0039a97
ISAWASHUN <syun20000616@icloud.com>
2024-04-06 17:08:35 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -5,22 +5,50 @@ import axios from 'axios';
 function App() {
 
   const [characters, setCharacters] = useState([]);
-
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    fetchCharacters();
+    fetchCharacters(2);
   }, [])
 
-  const fetchCharacters = async () => {
+  const limit = 15;
+
+  const fetchCharacters = async (page) => {
     const apiUrl = 'https://narutodb.xyz/api/character';
-    const result = await axios.get(apiUrl);
+    setIsLoading(true);
+    const result = await axios.get(apiUrl, {
+      params: {
+        page,
+        limit
+      }
+    });
     setCharacters(result.data.characters);
+    setIsLoading(false);
+  }
+
+  const handleNext = async () => {
+    const nextPage = page + 1;
+    await fetchCharacters(nextPage);
+    setPage(nextPage);
+  }
+
+  const handlePrev = async () => {
+    const prevPage = page - 1;
+    await fetchCharacters(prevPage);
+    setPage(prevPage);
   }
 
   return (
     <div className="container">
+      <div className="header">
+        <div className="header-content">
+          <img src="logo.png" alt="logo" className='logo'/>
+        </div>
+      </div>
+      {isLoading ? ( <div className="loading">Loading...</div> ): (
       <main>
-        <div className='cards-container'>
+        <div className='card-container'>
           {characters.map((character) => (
             <div key={character.id} className='card'>
               <img src={character.images[0] != null ? character.images[0] : 'dummy.png'} alt={character.name} />
@@ -38,7 +66,13 @@ function App() {
             </div>
           ))}
         </div>
+        <div className="pager">
+          <button className="prev" disabled={page === 1} onClick={handlePrev}>Previous</button>
+          <span className="page-number">{page}</span>
+          <button className="next" disabled={limit > characters.length} onClick={handleNext}>Next</button>
+        </div>
       </main>
+      )}
     </div>
   );
 }
